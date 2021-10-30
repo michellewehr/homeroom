d5040427684cd8a9010fae08c67bc67b614f0c4f
Michelle Wehr <michelle_wehr@icloud.com>
2021-10-30 14:16:52 -0400

    user able to add lesson plans

--- controllers/lesson-plans.js
+++ controllers/lesson-plans.js
@@ -28,6 +28,24 @@ router.get('/addLessonPlan', (req, res) => {
     res.render('addLessonPlan');
 })
 
+// add a lesson plan--had to add the url-- TODO: go back and fix this to be correct
+router.post('/api/lessonplans', ({ body }, res) => {
+    LessonPlan.create({
+        lesson_date: body.lesson_date,
+        subject_id: body.subject_id,
+        lesson_name: body.lesson_name,
+        lesson_objective: body.lesson_objective,
+        lesson_activity: body.lesson_activity,
+        materials: body.materials
+    })
+        .then(dbLessonPlanData => res.json({ msg: `Successfully added new lesson plan!` }))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
 //get lesson by user selection filter by subject
 router.get('/filterSub/:userSelection', (req, res) => {
     LessonPlan.findAll({
@@ -75,5 +93,6 @@ router.get('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
 
 module.exports = router;
