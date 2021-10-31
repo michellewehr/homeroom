325108de7ff25cfd7a4c6450505812a444da2df6
Brent Gaines <84875271+brentocracy@users.noreply.github.com>
2021-10-31 16:55:34 -0400

    Fix password validation test unit to pass multiple true and false examples

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,12 +2,10 @@ module.exports = {
     format_name: name => {
         name = name.trim()
         return `${name[0].toUpperCase()}${name.slice(1).toLowerCase()}`
-    }
-}
+    },
 
-module.exports = {
     validatePasswordConstraints: password => {
-        const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-        return password.match(regex) ? true : false;
+        const regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+        return regex.test(password);
     }
-}
+};
