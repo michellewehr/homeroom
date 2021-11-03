583441bbe7244f321fd584b0f524c3109911067d
Brent Gaines <84875271+brentocracy@users.noreply.github.com>
2021-11-03 18:03:46 -0400

    User redirected to dashboard when logged in and accessing login/signup pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,24 +4,22 @@ const sequelize = require('../config/connection');
 
 router.get('/', (req, res) => {
     res.render('homepage')
-})
+});
 
 router.get('/signup', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/dashboard');
     } else {
-        next();
+        res.render('signup')
     }
-    res.render('signup')
-})
+});
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/dashboard');
     } else {
-        next();
+        res.render('login');
     }
-    res.render('login');
-})
+});
 
 module.exports = router;
