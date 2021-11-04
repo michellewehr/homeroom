8ad295b314cd87cf1fc4352b138c4e882f37ad0f
Brent Gaines <84875271+brentocracy@users.noreply.github.com>
2021-11-04 10:37:50 -0400

    Fix post/get routes for lesson plan and grade rendering
    Assignment assessment value changed to lesson name value for clarity

--- controllers/gradebook-routes.js
+++ controllers/gradebook-routes.js
@@ -34,12 +34,12 @@ router.get('/addgrade', withAuth, (req, res) => {
             Assignment.findAll({
                 include: [{
                     model: Subject,
-                    // where: {
-                    //     teacher_subj_id: req.session.teacher_id
-                    // },
                     attributes: [['id', 'subject_id'], 'subject_name'],
                 }],
-                attributes: ['id', 'assignment_name'],
+                attributes: ['id', 'assignment_name', 'teacher_assign_id'],
+                where: {
+                    teacher_assign_id: req.session.teacher_id
+                },
                 order: [['subject_id', 'ASC']]
             })
                 .then(dbAssignmentData => {
@@ -58,26 +58,26 @@ router.get('/addgrade', withAuth, (req, res) => {
 });
 
 //ENGLISH 
-    router.get('/1', withAuth, (req, res) => {
-        Student.findAll({
-            where: {
-                teacher_id: req.session.teacher_id
-            },
-            order: [['last_name', 'ASC']],
-            include: [{
-                model: Grade,
-                attributes: ['number_grade'],
-                include: [
-                    {
-                        model: Assignment,
-                        attributes: ['assignment_name', 'subject_id'],
-                        where: {
-                            subject_id: 1,
-                       }
+router.get('/1', withAuth, (req, res) => {
+    Student.findAll({
+        where: {
+            teacher_id: req.session.teacher_id
+        },
+        order: [['last_name', 'ASC']],
+        include: [{
+            model: Grade,
+            attributes: ['number_grade'],
+            include: [
+                {
+                    model: Assignment,
+                    attributes: ['assignment_name', 'subject_id'],
+                    where: {
+                        subject_id: 1,
                     }
-                ]
-            }],
-        })
+                }
+            ]
+        }],
+    })
         .then(function (dbStudentData) {
             Assignment.findAll({
                 where: {
@@ -180,34 +180,34 @@ router.get('/3', withAuth, (req, res) => {
             ]
         }],
     })
-    .then(function (dbStudentData) {
-        Assignment.findAll({
-            where: {
-                subject_id: 3,
-            },
-            include: [{
-                model: Subject,
-                attributes: [['id', 'subject_id'], 'subject_name'],
-            }],
-            attributes: ['id', 'assignment_name'],
-            order: [['subject_id', 'ASC']]
+        .then(function (dbStudentData) {
+            Assignment.findAll({
+                where: {
+                    subject_id: 3,
+                },
+                include: [{
+                    model: Subject,
+                    attributes: [['id', 'subject_id'], 'subject_name'],
+                }],
+                attributes: ['id', 'assignment_name'],
+                order: [['subject_id', 'ASC']]
+            })
+                .then(dbAssignmentData => {
+                    const students = dbStudentData.map(student => student.get({ plain: true }));
+                    const assignments = dbAssignmentData.map(assignment => assignment.get({ plain: true }));
+                    const studentsAndAssignments = {
+                        students,
+                        assignments
+                    }
+                    console.log(studentsAndAssignments);
+                    res.render('gradebookScience', { studentsAndAssignments, loggedIn: true });
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        msg: `Sorry, this one's on our end. Try again? Error: ${err}`
+                    });
+                })
         })
-            .then(dbAssignmentData => {
-                const students = dbStudentData.map(student => student.get({ plain: true }));
-                const assignments = dbAssignmentData.map(assignment => assignment.get({ plain: true }));
-                const studentsAndAssignments = {
-                    students,
-                    assignments
-                }
-                console.log(studentsAndAssignments);
-                res.render('gradebookScience', { studentsAndAssignments, loggedIn: true });
-            })
-            .catch(err => {
-                res.status(500).json({
-                    msg: `Sorry, this one's on our end. Try again? Error: ${err}`
-                });
-            })
-    })
 });
 
 
@@ -244,18 +244,18 @@ router.get('/4', withAuth, (req, res) => {
                 attributes: ['id', 'assignment_name'],
                 order: [['subject_id', 'ASC']]
             })
-            .then(dbAssignmentData => {
-                const students = dbStudentData.map(student => student.get({ plain: true }));
-                const assignments = dbAssignmentData.map(assignment => assignment.get({plain: true}));
-                const studentsAndAssignments = {
-                    students,
-                    assignments
-                }
-                res.render('gradebookSocialStudies', {studentsAndAssignments, loggedIn: true});
-            })
-            .catch(err => {
-                res.status(500).json(err);
-            })
+                .then(dbAssignmentData => {
+                    const students = dbStudentData.map(student => student.get({ plain: true }));
+                    const assignments = dbAssignmentData.map(assignment => assignment.get({ plain: true }));
+                    const studentsAndAssignments = {
+                        students,
+                        assignments
+                    }
+                    res.render('gradebookSocialStudies', { studentsAndAssignments, loggedIn: true });
+                })
+                .catch(err => {
+                    res.status(500).json(err);
+                })
         })
 });
 
