75c76d784dce05d801ea578ede990509b6af47ca
Michelle Wehr <michelle_wehr@icloud.com>
2021-11-03 21:29:03 -0400

    lesson plans able to add again

--- controllers/lesson-plans.js
+++ controllers/lesson-plans.js
@@ -15,13 +15,14 @@ router.get('/', withAuth, (req, res) => {
         ],
          include: [{
             model: Subject,
-            where: {
-                teacher_subj_id: req.session.teacher_id
-            },
+            // where: {
+            //     teacher_subj_id: req.session.teacher_id
+            // },
             attributes: ['subject_name'],
         }]
     })
         .then(dbLessonPlanData => {
+            console.log(dbLessonPlanData);
             const lessons = dbLessonPlanData.map(lesson => lesson.get({ plain: true }));
             res.render('lesson-plans', {
                 lessons,
@@ -50,16 +51,16 @@ router.get('/addLessonPlan', withAuth, (req, res) => {
 })
 
 // add a lesson plan--had to add the url-- TODO: go back and fix this to be correct
-router.post('/api/lessonplans', withAuth, (req, res) => {
-    LessonPlan.create({
-        lesson_date: req.body.lesson_date,
-        subject_id: req.body.subject_id,
-        lesson_name: req.body.lesson_name,
-        lesson_objective: req.body.lesson_objective,
-        lesson_activity: req.body.lesson_activity,
-        materials: req.body.materials,
-        teacher_lesson_id: req.session.teacher_id
-    })
+    router.post('/', withAuth, ({ body }, res) => {
+        LessonPlan.create({
+            lesson_date: body.lesson_date,
+            subject_id: body.subject_id,
+            lesson_name: body.lesson_name,
+            lesson_objective: body.lesson_objective,
+            lesson_activity: body.lesson_activity,
+            materials: body.materials,
+            teacher_lesson_id: req.session.teacher_id
+        })
         .then(dbLessonPlanData => {
             res.json({ msg: `Successfully added new lesson plan!` })
             console.log(dbLessonPlanData + 'line 47 created');
